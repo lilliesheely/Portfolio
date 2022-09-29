d88a99712cfacf105a5241dacfc97d1c5e507ded
lilliesheely <lilliesheely@gmail.com>
2022-09-29 12:27:37 -0700

    Remove Token

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,9 @@
-import { Link } from 'react-router-dom';
-import * as userService from '../../utilities/users-service'
-
-export default function NavBar({user, setUser}) {
-  function handleLogOut() {
-    userService.logOut(); 
-    setUser(null); 
-  }
+export default function NavBar() {
   return (
     <nav>
-      <Link to='/orders'>Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to='/orders/new'>New Order</Link>
-      &nbsp; | &nbsp;
-      Welcome, {user.name}!
-      &nbsp; | &nbsp;
-      <Link to='' onClick={handleLogOut} >Log Out</Link>
+      <div>About Me</div>
+      <div>Resume</div>
+      <div>Projects</div>
     </nav>
   );
 }
--- src/pages/OrderHistoryPage/OrderHistoryPage.jsx
+++
@@ -1,9 +0,0 @@
-import { checkToken } from "../../utilities/users-service"
-
-export default function OrderHistoryPage() {
-  return (
-    <>
-      <h1>OrderHistoryPage</h1>
-    </>
-  );
-}
